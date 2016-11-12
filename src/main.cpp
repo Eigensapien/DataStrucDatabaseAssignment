5ef071b1f2829517771ffed5f5eb2710032e3b13
Eric Freda <eric.freda@yahoo.com>
2016-11-12 06:45:04 +0000

    #include all files in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,11 @@
 Should have a way of navigating menus after doing File I/O stuff.
 Basically just calling functions from the other files inside a loop of some sort
 */
+
+#include <iostream>
+#include "./BSTree/bstree.h"
+#include "./Person/Student/student.h"
+#include "./Person/Faculty/faculty.h"
+#include "./FileIO/fileio.h"
+#include "./DBfuncs/dbfuncs.h"
+#include "./RollbackStack/rollbackstack.h"
