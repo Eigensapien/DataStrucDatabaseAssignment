dae7c683512d2ba3dedf9959400ef6df3f463721
Eric Freda <eric.freda@yahoo.com>
2016-11-14 19:55:07 +0000

    rought draft import functions

--- src/FileIO/fileio.cpp
+++ src/FileIO/fileio.cpp
@@ -23,6 +23,139 @@
 //if error is found, exit. tell user to fix files or delete them
 
 
+//need to parse: strings, ints
+
+
+
+//this function returns an int error code and modifies its first argument directly by adding stuff from the file to a (hopefully) empty tree
+int importStudentTree(BSTree<Student> *tree, string filename)
+{
+    ifstream ifs;
+    
+    ifs.open( filename ); //open the file
+    
+    if (!ifs)                       //if the given filename doesn't work,
+    {                               //output error and exit function without importing.
+        if (ifs.is_open()) { ifs.close(); };
+        cout << "Error: Student file cannot be opened." << endl;
+        return 1;
+    }
+    
+    string currLine;
+    getline(ifs, currLine); //First line should be "_STARTSTUDENT_". This gives a way to know when to stop.
+    
+    if (currLine != "_STARTSTUDENT_")
+    {                               //output error and exit function without importing.
+        if (ifs.is_open()) { ifs.close(); };
+        cout << "Error: Student file does not begin with _STARTSTUDENT_ ." << endl;
+        return 1;
+    }
+    
+    Student stu;
+    
+    while (currLine=="_STARTSTUDENT_") //ie. until we run out of students
+    {
+        getline(ifs, currLine); // ID number
+        stu.ID = std::stoi(currLine); //need to make or find this
+        
+        getline(ifs, stu.name); //name
+        getline(ifs, stu.level); //level
+        getline(ifs, stu.major); //major
+        
+        getline(ifs, currLine); //GPA
+        stu.GPA = std::stod(currLine); //need to make this or find this
+        
+        getline(ifs,currLine); //advisor
+        stu.advisor = std::stoi(currLine); //need to make or find this
+        
+        tree->insert(stu); //place them in the tree
+        
+        getline(ifs, currLine); // "_STARTSTUDENT_" if there is another, blank otherwise
+    }
+    
+    std::cout << "Student data has been imported." << std::endl;
+}
+
+
+
+
+
+
+//this function returns an int error code and modifies its first argument directly by adding stuff from the file to a (hopefully) empty tree
+int importStudentTree(BSTree<Student> *tree, string filename)
+{
+    ifstream ifs;
+    
+    ifs.open( filename ); //open the file
+    
+    if (!ifs)                       //if the given filename doesn't work,
+    {                               //output error and exit function without importing.
+        if (ifs.is_open()) { ifs.close(); };
+        cout << "Error: Faculty file cannot be opened." << endl;
+        return 1;
+    }
+    
+    string currLine;
+    getline(ifs, currLine); //First line should be "_STARTFACULTY_". This gives a way to know when to stop.
+    
+    if (currLine != "_STARTFACULTY_")
+    {                               //output error and exit function without importing.
+        if (ifs.is_open()) { ifs.close(); };
+        cout << "Error: Faculty file does not begin with _STARTFACULTY_ ." << endl;
+        return 1;
+    }
+    
+    Faculty fac;
+    int numStu;
+    
+    while (currLine=="_STARTFACULTY_") //ie. until we run out of faculty
+    {
+        getline(ifs, currLine); // ID number
+        fac.ID = std::stoi(currLine); //need to make or find this
+        
+        getline(ifs, fac.name); //name
+        getline(ifs, fac.level); //level
+        getline(ifs, fac.dept); //dept
+        
+        getline(ifs, currLine); //number of students
+        numStu = std::stoi(currLine); //need to make this or find this
+        
+        for (int i=0; i<numStu; ++i) //loop over list of students
+        {
+            getline(ifs, currLine); //student's ID
+            fac.students.insert( std::stoi(currLine) );
+        }
+        
+        tree->insert(fac); //put them in the tree
+        
+        getline(ifs, currLine); // "_STARTFACULTY_" if there are more, blank otherwise
+    }
+    std::cout << "Faculty data has been imported." << std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #include <iostream>
@@ -35,15 +168,18 @@ void saveStudentTreeToFile(BSTree<Student> *tree, std::string filename)
 	If we use pre-order traversal, then everything will
 	be written to the file in such a manner that reading
 	the file in the order it is written reconstructs the
-	tree exactly. Thus, the output file should look like:
+	tree exactly. We also need an identifier so that the
+    import function has information about when to stop.
+    Thus, the output file should look like:
 	
-	01208230981	//ID#
-	Billy Hill	//name
-	Freshman	//level
-	Psychology	//major
-	3.75		//gpa
-	71238924789	//advisorid
-	65154651234	//next student
+    _STARTSTUDENT_ //indicator
+	01208230981	   //ID#
+	Billy Hill	   //name
+	Freshman	   //level
+	Psychology	   //major
+	3.75		   //gpa
+	71238924789	   //advisorid
+	65154651234	   //next student
 	Joe Schmo
 	...
 	*/
@@ -63,6 +199,7 @@ void saveStuRecur(TreeNode<Student> *root, ofstream ofs)
 
 void stuToFile(Student stu, ofstream ofs);
 {
+    ofs << "_STARTSTUDENT_" << std::endl;
 	ofs << stu.ID << std::endl;
 	ofs << stu.name << std::endl;
 	ofs << stu.level << std::endl;
@@ -80,16 +217,18 @@ void saveFacultyTreeToFile(BSTree<Faculty> *tree, std::string filename)
 	If we use pre-order traversal, then everything will
 	be written to the file in such a manner that reading
 	the file in the order it is written reconstructs the
-	tree exactly. Thus, the output file should look like:
+	tree exactly. An indicator helps with file import. 
+    Thus, the output file should look like:
 	
-	78945312564	//ID#
-	Jeff Dahmer	//name
-	Professor	//level
-	Psychology	//dept
-	2			//number of students
-	45645645665	//first student
-	65154651234	//next student
-	13215484651 //id of next faculty member
+    _STARTFACULTY_ //indicator
+	78945312564	   //ID#
+	Jeff Dahmer	   //name
+	Professor	   //level
+	Psychology	   //dept
+	2			   //number of students
+	45645645665	   //first student
+	65154651234	   //next student
+	13215484651    //id of next faculty member
 	Snoop Dogg
 	...
 	*/
@@ -117,7 +256,7 @@ void facToFile(Faculty *fac, ofstream ofs)
 	facStuToFile(fac, ofs); //this function should go through the list and save each students id number
 }
 
-facStuToFile(Faculty *fac, ofstream ofs)
+void facStuToFile(Faculty *fac, ofstream ofs)
 {
 	ListNode<int> *curr = fac->students->front; //make sure this matches the list implimentation and list has front as protected, not private
 	for (int i=0; i<fac->numStudents; ++i)
